f1e7bd5705a02e6610cd638e2715b98e6146caa2
aznprodude12 <hoa_ho123@yahoo.com>
2022-12-14 13:44:44 -0600

    Add Location and Import Font
    This commit adds the location component and import Poppins font from Google Fonts.

---
+++ src/components/TimeAndLocation.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+
+function TimeAndLocation() {
+     return <div>
+          <div className="flex items-center justify-center my-6">
+               <p className="text-white text-xl font-extralight">
+                    Tuesday, 14 December 2022 | Local time: 12:52 PM
+               </p>
+          </div>
+
+          <div className="flex items-center justify-center my-3">
+               <p className="text-white text-3xl font-medium">
+                    West Fargo, ND
+               </p>
+          </div>
+     </div>
+}
+
+export default TimeAndLocation
