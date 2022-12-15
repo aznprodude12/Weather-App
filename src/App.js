8d47e3f2b5a7f5a9ade42a4b8f6a5ec093eb6606
aznprodude12 <hoa_ho123@yahoo.com>
2022-12-15 10:58:12 -0600

    Add Temperature Details Component
    This commit add temperature details such as:
    - Real Feel Temperature
    - Humidity
    - Sunrise Time
    - Sunset Time
    - High Temperature
    - Low Temperature

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import UilReact from '@iconscout/react-unicons/icons/uil-react'
 import TopButtons from './components/TopButtons'
 import Inputs from './components/Inputs'
 import TimeAndLocation from './components/TimeAndLocation'
+import TemperatureAndDetails from './components/TemperatureAndDetails'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
               <Inputs />
 
               <TimeAndLocation />
+              <TemperatureAndDetails />
          </div>
     </div>
   );
