fea472518232a0631891ffb3cda1b0de46abfadd
aznprodude12 <hoa_ho123@yahoo.com>
2022-12-18 17:49:36 -0600

    Display Temperature Data
    This commit displays the temperature data for:
    - Current temperature
    - Real feel temperature
    - Humidity
    - Wind Speed
    - Sunrise time
    - Sunset Time
    - High Temperature for the day
    - Lowest Temperature for the day

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import getFormattedWeatherData from './services/weatherService'
 
 function App() {
 
-     const [ query, setQuery ] = useState({ q: 'berlin' })
+     const [ query, setQuery ] = useState({ q: 'fargo' })
      const [ units, setUnits ] = useState('metric')
      const [ weather, setWeather ] = useState(null)
 
