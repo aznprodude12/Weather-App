24cf6b4da29e115043c040899a03648069b669d2
aznprodude12 <hoa_ho123@yahoo.com>
2022-12-19 21:44:32 -0600

    Change Background Color Based on Temperature
    This commit changes the background color based on the temperature of the location. For Celsius, if the temperature is below 20, background color set to blue. Above 20, set to orange. For Fahrenheit, the threshold is 60.

--- src/App.js
+++ src/App.js
@@ -25,9 +25,21 @@ function App() {
           fetchWeather()
      }, [query, units])
 
+     const formatBackground = () => {
+          if (!weather)
+               return 'from-cyan-700 to-blue-700'
+
+          const threshold = units === 'metric' ? 20 : 60
+
+          if (weather.temp <= threshold)
+               return 'from-cyan-700 to-blue-700'
+          else
+               return 'from-yellow-700 to-orange-700'
+     }
+
      return (
           <div className="App">
-              <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
+              <div className={`mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400 ${formatBackground()}`}>
                    <TopButtons setQuery={setQuery} />
                    <Inputs
                         setQuery={setQuery}
